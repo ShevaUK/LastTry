5fb8d3956ab8ef803d0bc62120d4345373a625a2
ShevaUK <shevchuck00777@gmail.com>
2023-04-12 18:57:24 +0300

    no coments7

--- src/main/java/org/example/LastController.java
+++ src/main/java/org/example/LastController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LastController {
-    @GetMapping("/welcome")
+    @GetMapping("/")
     public String welcome(){
             return "Welcome to spring boot heroku demo";
         }
