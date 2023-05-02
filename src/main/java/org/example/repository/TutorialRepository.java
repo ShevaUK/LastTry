a1bb9b0951454937ecd409f1b0522b3f9427a791
ShevaUK <shevchuck00777@gmail.com>
2023-05-02 13:13:18 +0300

    71

---
+++ src/main/java/org/example/repository/TutorialRepository.java
@@ -0,0 +1,23 @@
+package org.example.repository;
+// new class
+import java.util.List;
+import java.util.Optional;
+
+import jakarta.validation.constraints.NotNull;
+import org.example.entity.Tutorial;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+public interface TutorialRepository extends MongoRepository<Tutorial, String>{
+    Page<Tutorial> findByPublished(boolean published, Pageable pageable);
+
+    Page<Tutorial> findByTitleContainingIgnoreCase(String title, Pageable pageable);
+    @Query("{'title': ?0}")
+    Optional<Tutorial> findByTitel(String tutorial);
+    List<Tutorial> findByTitleContaining(String title, Sort sort);
+    @NotNull
+    Optional<Tutorial> findById(Tutorial tutorial,String id);
+}
