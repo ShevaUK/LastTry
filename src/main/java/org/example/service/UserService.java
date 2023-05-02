c79a450a7a8e48acd39553840e36ab7650e5fbdf
ShevaUK <shevchuck00777@gmail.com>
2023-05-02 14:38:21 +0300

    81

--- src/main/java/org/example/service/UserService.java
+++ src/main/java/org/example/service/UserService.java
@@ -2,8 +2,15 @@ package org.example.service;
 
 
 import org.example.dto.UserDTO;
+import org.example.entity.User;
 import org.example.utils.BaseResponseDTO;
 
 public interface UserService {
     BaseResponseDTO registerAccount(UserDTO userDTO);
+    User getUserById(String userId);
+
+    User addUser(User user);
+
+    User updateUser(User user);
+    User addTutorialById(String userId, String tutorialId);
 }
--- src/main/java/org/example/service/impl/UserServiceImpl.java
+++ src/main/java/org/example/service/impl/UserServiceImpl.java
@@ -5,9 +5,12 @@ package org.example.service.impl;
 import lombok.RequiredArgsConstructor;
 import org.example.dto.UserDTO;
 import org.example.entity.Role;
+import org.example.entity.Tutorial;
 import org.example.entity.User;
 import org.example.exception.BaseException;
+import org.example.exception.ResourceNotFoundException;
 import org.example.repository.RoleRepository;
+import org.example.repository.TutorialRepository;
 import org.example.repository.UserRepository;
 import org.example.service.UserService;
 import org.example.utils.BaseResponseDTO;
@@ -16,6 +19,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -29,6 +33,7 @@ public class UserServiceImpl implements UserService {
     private final RoleRepository roleRepository;
 
     private final BCryptPasswordEncoder passwordEncoder;
+    private final TutorialRepository tutorialRepository;
 
     @Override
     public BaseResponseDTO registerAccount(UserDTO userDTO) {
@@ -78,4 +83,36 @@ public class UserServiceImpl implements UserService {
             throw new BaseException(String.valueOf(HttpStatus.BAD_REQUEST.value()), "Invalid role");
         }
     }
+    @Override
+    public User getUserById(String userId) {
+        return userRepository.findById(userId).orElse(null);
+    }
+    @Override
+    public User addUser(User user) {
+        return userRepository.save(user);
+    }
+    @Override
+    public User updateUser(User user) {
+        return userRepository.save(user);
+    }
+    @Override
+    public User addTutorialById(String userId, String tutorialId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User", "id", userId));
+        Tutorial tutorial = tutorialRepository.findById(tutorialId)
+                .orElseThrow(() -> new ResourceNotFoundException("Tutorial", "id", tutorialId));
+        // create tutorials list if it doesn't exist
+        List<Tutorial> tutorials = user.getTutorials();
+        if (tutorials == null) {
+            tutorials = new ArrayList<>();
+        }
+
+        // add tutorial to user's tutorial list
+        tutorials.add(tutorial);
+        user.setTutorials(tutorials);
+        userRepository.save(user);
+
+        return user;
+    }
+
 }
