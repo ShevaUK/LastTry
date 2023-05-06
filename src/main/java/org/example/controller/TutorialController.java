8025529bb578e3ce0ffe05ea037af0cce8862f53
ShevaUK <shevchuck00777@gmail.com>
2023-05-06 20:40:29 +0300

    92

--- src/main/java/org/example/controller/TutorialController.java
+++ src/main/java/org/example/controller/TutorialController.java
@@ -16,7 +16,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
-//@CrossOrigin(origins = "https://front-book-tutorials.herokuapp.com")
 @RestController
 @RequestMapping("/guest")
 public class TutorialController {
--- src/main/java/org/example/controller/UserController.java
+++ src/main/java/org/example/controller/UserController.java
@@ -20,15 +20,7 @@ import org.example.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.security.Principal;
 import java.util.List;
 
@@ -67,7 +59,7 @@ public class UserController {
 
     }
 
-    @GetMapping("WWW")
+    @GetMapping("/WWW")
     public String welcome() {
         return "Welcome to spring boot heroku demo";
     }
