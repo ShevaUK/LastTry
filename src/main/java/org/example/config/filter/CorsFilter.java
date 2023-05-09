e98abad73e95caab1612bb758ad59bc235da6e31
ShevaUK <shevchuck00777@gmail.com>
2023-05-09 15:02:52 +0300

    109

--- src/main/java/org/example/config/filter/CorsFilter.java
+++ src/main/java/org/example/config/filter/CorsFilter.java
@@ -5,7 +5,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.stereotype.Component;
+
 
 import java.io.IOException;
 /////////
