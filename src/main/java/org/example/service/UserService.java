a02248eb45423269b9e3ebb48825e7175889ddaa
ShevaUK <shevchuck00777@gmail.com>
2023-05-19 14:01:40 +0300

    127 with new upload image

--- src/main/java/org/example/service/UserService.java
+++ src/main/java/org/example/service/UserService.java
@@ -2,7 +2,6 @@ package org.example.service;
 
 
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.example.dto.UserDTO;
 import org.example.entity.Tutorial;
 import org.example.entity.User;
@@ -10,10 +9,8 @@ import org.example.utils.BaseResponseDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
 import java.util.List;
-import java.util.Map;
+
 
 public interface UserService {
     public User getCurrentUser(HttpServletRequest request);
